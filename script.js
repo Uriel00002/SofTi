1ef9a145219fde324e63b3072a4e529dda1443bd
javilacg <j.avila@cuatitos.com.mx>
2024-10-05 12:18:34 -0600

    header responsive

--- script.js
+++ script.js
@@ -4,36 +4,32 @@ const carousel = document.getElementById('carousel');
 let currentIndex = 0;
 let interval;
 
-// Function to show the current card
 function showCard(index) {
     cards.forEach((card, i) => {
         card.classList.toggle('active', i === index);
     });
     buttons.forEach((button, i) => {
-        button.classList.toggle('bg-gray-500', i === index); // Change button style for active card
+        button.classList.toggle('bg-gray-500', i === index);
     });
     carousel.style.backgroundImage = `url(./assets/imgs/img${index+1}.jpg)`;
 }
 
-// Function to go to the next card
 function nextCard() {
     currentIndex = (currentIndex + 1) % cards.length;
     showCard(currentIndex);
 }
 
-// Event listener for buttons
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        clearInterval(interval); // Stop auto rotation when button is clicked
+        clearInterval(interval);
         currentIndex = parseInt(button.getAttribute('data-index'));
         showCard(currentIndex);
-        startCarousel(); // Restart auto rotation
+        startCarousel();
     });
 });
 
-// Start the carousel automatically
 function startCarousel() {
-    interval = setInterval(nextCard, 3000); // Change card every 3 seconds
+    interval = setInterval(nextCard, 3000);
 }
 
 // Initialize
